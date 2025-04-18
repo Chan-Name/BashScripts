df67cd027e00565ede338192fa43822ff78340b3
Chan-Name <ivan.shakhov.08@gmail.com>
2025-04-18 16:09:37 +0300

    Change First script

--- webm_to_mp4.sh
+++ webm_to_mp4.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-F_DIR="~/Downloads"
-L_DIR="~/VIDEO"
+F_DIR="$HOME/Downloads"
+L_DIR="$HOME/VIDEO"
 
 webm_to_mp4() {
 
-    if ! command -v ffmpeg &>/dev/null; then
-        echo "Pls install ffmpeg"
-    else
-        for file_webm in *.webm; do
-            if [[ -f "$file_webm" ]]; then
-                file_mp4 = "${file_webm%.webm}.mp4"
-                ffmpeg -i $file $file_mp4 && rm $file_webm
-            fi
-        done
+    if command -v ffmpeg &>/dev/null; then
+        echo "install ffmpeg pls"
+        return 1
     fi
+
+    for file_webm in *.webm; do
+        if [[ -f "$file_webm" ]]; then
+            file_mp4="${file_webm%.webm}.mp4"
+            ffmpeg -i $file $file_mp4 && rm $file_webm
+        fi
+    done
 }
 
 mp4_to_dir() {
 
     mkdir -p $L_DIR
 
-        for file_mp4 in *.mp4; do
-            if [[ -f "$file_mp4" ]]; then
-                mv $file_mp4 $L_DIR
-            fi
-        done
-    }
+    for file_mp4 in *.mp4; do
+        if [[ -f "$file_mp4" ]]; then
+            mv $file_mp4 $L_DIR
+        fi
+   done
+}
 
-    main() {
-        cd $F_DIR 2>/dev/null || { echo "Ошибка: Директория ~/$F_DIR не найдена!"; exit 1; }
+main() {
+    cd $F_DIR 2>/dev/null || { echo "Ошибка: Директория ~/$F_DIR не найдена!"; exit 1; }
     webm_to_mp4
     mp4_to_dir
     echo "Webm to mp4 and move to ${L_DIR}"
 }
+
+main
